736764ddf8dc0b6c08bf11b3f280f00c76b0bf1c
amirreza1374 <126147703+amirreza1374@users.noreply.github.com>
2023-12-25 09:32:26 +0330

    remove: additional code

--- src/components/context/HotelProvider.jsx
+++ src/components/context/HotelProvider.jsx
@@ -35,7 +35,7 @@ export default function HotelProvider({ children }) {
       value={{
         isLoading,
         hotelsData,
-        getHotel,
+        
         currentHotel,
         getHotel,
         isLoadingCurrHotel,
